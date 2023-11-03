89b0d436898d5db05e74b0fc5c36ebb25abb3998
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2023-11-03 11:21:38 +0900

    ui 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: '리액트1',
+      body: '내용1',
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: '리액트2',
+      body: '내용2',
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: '리액트3',
+      body: '내용3',
+      isDone: true,
+    },
+  ]);
   return (
     <div className='App'>
       <h1>My Todo List</h1>
@@ -14,10 +35,34 @@ function App() {
 
       <section>
         <h2>Working... 🔥</h2>
+        {todos.map((todo) => {
+          if (!todo.isDone) {
+            return (
+              <div key={todo.id}>
+                <h3>{todo.title}</h3>
+                <p>{todo.body}</p>
+                <button>삭제하기</button>
+                <button>완료</button>
+              </div>
+            );
+          }
+        })}
       </section>
 
       <section>
         <h2>Done..! 🎉</h2>
+        {todos.map((todo) => {
+          if (todo.isDone) {
+            return (
+              <div key={todo.id}>
+                <h3>{todo.title}</h3>
+                <p>{todo.body}</p>
+                <button>삭제하기</button>
+                <button>완료</button>
+              </div>
+            );
+          }
+        })}
       </section>
     </div>
   );
